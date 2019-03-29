5d3a9e23407825ee2e9a3302f7d9a96dcac3b80d
Kyle Girard <kyle@kdmanalytics.com>
2019-03-29 13:12:52 -0400

    Remove extra import

--- src/main/java/com/kdma/auth/AuthApplication.java
+++ src/main/java/com/kdma/auth/AuthApplication.java
@@ -7,7 +7,6 @@ import javax.annotation.PostConstruct;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 public class AuthApplication {
